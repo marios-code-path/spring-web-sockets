bdeb65344e5517e2c470079b5d0d5e2945baf1fe
Mario Gray <mario.gray@gmail.com>
2018-03-26 14:40:02 -0700

    wip

---
+++ src/main/java/com/example/springsockets/SpringSocketsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocketsApplication.class, args);
+	}
+}
