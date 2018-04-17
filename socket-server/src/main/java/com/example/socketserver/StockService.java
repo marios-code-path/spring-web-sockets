f652117c32ac9f392e09041259ec68058ffbcd51
Mario Gray <mario.gray@gmail.com>
2018-04-16 21:27:39 -0700

    fixed a bug

--- socket-server/src/main/java/com/example/socketserver/StockService.java
+++ socket-server/src/main/java/com/example/socketserver/StockService.java
@@ -10,17 +10,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 public class StockService {
-    double state = 1.0;
 
     private final Collection<String> symbolUniverse = Collections.singletonList("PVTL");
-
-//    Flux<String> flux = Flux.generate(
-//            () -> 0,
-//            (state, sink) -> {
-//                sink.next("3 x " + state + " = " + 3*state);
-//                if (state == 10) sink.complete();
-//                return state + 1;
-//            });
 
     Flux<Stock> getStream(String ticker) {
         return Flux.generate(
@@ -32,18 +23,8 @@ public class StockService {
                 });
     }
 
-    /**
-     *                  Flux.generate(
-     (Consumer<SynchronousSink<WebSocketMessage>>)
-     sink -> sink.next(session.textMessage("Hello " + System.currentTimeMillis()))
-     ).delayElements(Duration.ofSeconds(1))
-     * @return
-     */
-
     private double randomDelta() {
-        state += ThreadLocalRandom.current().nextDouble(-2.0, 5.0);
-
-        return 0.0;
-    };
+        return ThreadLocalRandom.current().nextDouble(-2.0, 5.0);
+    }
 
 }
