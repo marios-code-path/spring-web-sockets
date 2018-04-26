5c2abf82524b8faa360d9b168ecef76a5689a2a0
Mario Gray <mario.gray@gmail.com>
2018-04-26 15:01:10 -0700

    ref-wip

---
+++ socket-server/src/main/java/com/example/socketserver/ServerApplication.java
@@ -0,0 +1,11 @@
+package com.example.socketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
